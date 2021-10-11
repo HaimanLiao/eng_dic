b7bc86bda7c587148bb37a20ece507f2fbc59d49
Haiman Liao <haiman.liao@wbstar.com>
2021-10-10 23:01:50 -0700

    1 Change the name of one fuction of linear_file_operate from "linear_file_add" to "linear_file_append"; 2 Add file "List.c" and file "List.h" which are List operate interfaces;

---
+++ List.c
@@ -0,0 +1,100 @@
+#include "common.h"
+#include "List.h"
+#include <stdint.h>
+
+typedef int(*element_cmp)(char *, char *);
+typedef int(*element_delete)(char *);
+typedef int(*element_qualify)(char *);
+
+int List_init(List *list_hd)
+{
+	if(list_hd->ptr == NULL)return -1;
+
+	list_hd->ptr = (char *)malloc(list_hd->size);
+	memset(list_hd->ptr, 0, list_hd->size);//Implicitly assign the head part
+}
+
+int List_get_item_num(List *list_hd, uint32_t flag)
+{
+	if(list_hd->ptr == NULL)return -1;
+	
+	if(flag == 0)return list_hd->element_num;
+	else return list_hd->element_real_num;
+}
+
+int List_append_items(List *list_hd, char *buf, uint32_t num)
+{
+	if(list_hd->ptr == NULL)return -1;
+
+	char *tmp_ptr = (list_hd->ptr) + (list_hd->head_size) + (list_hd->element_real_num) * (list_hd->element_size);
+	memcpy(tmp_ptr, buf, num * (list_hd->element_size));//Could cause memory corrupt if the size allocated for ram isn't enough
+
+	(list_hd->element_real_num) += num;
+	(list_hd->element_num) += num;
+}
+
+int List_get_item_index(List *list_hd, char *name, element_cmp cmp)
+{
+	if(list_hd->ptr == NULL)return -1;
+
+	char *element_ptr = list_hd->ptr + list_hd->head_size;
+
+	for(int i = 0; i < list_hd->element_real_num; i++)
+	{
+		if(cmp(element_ptr, name) == 0)return i;
+		element_ptr += (list_hd->head_size);
+	}
+
+	return -1;
+}
+
+int List_delete_item(List *list_hd, uint32_t index, element_delete de_qualify)
+{
+	if(list_hd->ptr == NULL)return -1;
+	if(index >= (list_hd->element_real_num))return -1;
+
+	char *element_ptr = (list_hd->ptr) + (list_hd->head_size) + index * (list_hd->element_size);
+	if(de_qualify(element_ptr) == OK)
+	{
+		(list_hd->element_num)--;
+		return OK;
+	}
+
+	return -1;
+}
+
+int List_clean(List *list_hd, element_qualify is_qualify)
+{
+	char *new_ptr = malloc(list_hd->size);
+	memset(new_ptr, 0, list_hd->size);
+
+	//copy the head part
+
+	char *ptr1 = new_ptr + list_hd->head_size;
+	char *ptr2 = list_hd->ptr + list_hd->head_size;
+
+	for(int i = 0; i < (list_hd->element_real_num); i++)
+	{
+		if(is_qualify(ptr2))
+		{
+			memcpy(ptr1, ptr2, list_hd->element_size);
+			ptr1 += (list_hd->element_size);
+		}
+		ptr2 += (list_hd->element_size);
+	}
+
+	free(list_hd->ptr);
+	list_hd->ptr = new_ptr;
+	list_hd->element_real_num = list_hd->element_num;
+
+	return OK;
+}
+
+void List_uinit(List *list_hd)
+{	
+	if(list_hd->ptr == NULL)return;
+
+	free(list_hd->ptr);
+	list_hd->ptr = NULL;
+}
+
---
+++ List.h
@@ -0,0 +1,24 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdint.h>
+
+typedef struct List{
+	char	*ptr;		//Need to be initialized as NULL
+
+	uint32_t head_size;	//Need initialization
+	uint32_t element_size;	//Need initialization
+	uint32_t size;		//Need initialization (used as malloc parameter)
+	uint32_t flag;		//Need initialization (0)
+
+	uint32_t element_num;
+	uint32_t element_real_num;
+}
+
+int List_init(List *list_hd);
+int List_get_item_num(List *list_hd, uint32_t flag);
+int List_append_items(List *list_hd, char *buf, uint32_t num);
+int List_gei_item_index(List *list_hd, char *name, element_cmp cmp);
+int List_delete_item(List *list_hd, uint32_t index, element_delete dequalify);
+int List_clean(List *list_hd, element_qualify is_qualify);
+void List_uinit(List *list_hd);
